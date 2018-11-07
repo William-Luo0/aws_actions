33387db4ef1b13000e5b55dab8e482ca1cb5d91e
William Luo <none>
2018-11-07 22:59:28 +1100

    Added circleci config; Added xml tests and package requirements

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,7 @@
+import os
 import unittest
 
+import xmlrunner
+
 suite = unittest.TestLoader().discover('tests')
-tests = unittest.TextTestRunner(verbosity=2).run(suite)
+xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS', 'test-reports')).run(suite)
