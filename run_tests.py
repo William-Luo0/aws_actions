60643ac7bd6c5fa01ffdb2d8cc50cec5eff015ae
William Luo <none>
2018-11-08 22:57:27 +1100

    Update CircleCI testing

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,18 @@
 import os
+import shutil
 import unittest
 
 import xmlrunner
 
+# Constants
+_test_report_dir = 'test-reports'
+
+# Remove previous tests
+try:
+    shutil.rmtree(_test_report_dir)
+except FileNotFoundError:
+    pass
+
+# Run tests
 suite = unittest.TestLoader().discover('tests')
-xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS', 'test-reports')).run(suite)
+xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS', _test_report_dir)).run(suite)
