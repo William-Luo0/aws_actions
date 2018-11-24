607c98d0120fe21988143f2c3cb67f6a2fc9d713
William Luo <none>
2018-11-24 23:52:21 +1100

    Add policy permission expansion

---
+++ expand_permissions.py
@@ -0,0 +1,35 @@
+import fnmatch
+import json
+import sys
+
+
+def expand_permissions(aws_policy, all_actions):
+    policy_json = []
+    try:
+        policy_json = json.loads(aws_policy)
+
+    except (ValueError) as e:
+        print("ERR: Given 'aws_policy' is not valid JSON", file=sys.stderr)
+        exit(e)
+
+    # noinspection PyTypeChecker
+    for statement in policy_json["Statement"]:
+        replace_actions = []
+        for action in statement["Action"]:
+            matched_actions = fnmatch.filter(all_actions, action)
+            if len(matched_actions) == 0:
+                replace_actions.append(action)
+                print("WARN: Could not match '" + action + "'", file=sys.stderr)
+            else:
+                replace_actions.extend(matched_actions)
+        statement['Action'] = replace_actions
+    return json.dumps(policy_json)
+
+
+def main():
+    print("ERR: This module can't be directly run. Please import as a module.", file=sys.stderr)
+    exit(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_expand_permissions.py
@@ -0,0 +1,70 @@
+import unittest
+
+import expand_permissions
+
+
+class TestExpandPermissions(unittest.TestCase):
+    """
+    GIVEN an aws managed policy and a full list of possible actions
+    """
+
+    def test_unknown_service_expansion(self):
+        """
+        WHEN the given policy has a service/action that is not known
+        THEN the unknown service/action should be returned unchanged
+        """
+        test_policy = '{"Version": "2012-10-17", ' + \
+                      '"Statement": [{"Action": ["unknownService:*"], ' + \
+                      '"Effect": "Allow", ' + \
+                      '"Resource": "*"}]}'
+        test_actions = ['service:action1', 'service:action2', 'service:action3']
+
+        full_policy = expand_permissions.expand_permissions(test_policy, test_actions)
+
+        expected_policy = '{"Version": "2012-10-17", ' + \
+                          '"Statement": [{"Action": ["unknownService:*"], ' + \
+                          '"Effect": "Allow", ' + \
+                          '"Resource": "*"}]}'
+        self.assertEqual(full_policy, expected_policy)
+
+    def test_full_service_expansion(self):
+        """
+        WHEN the given policy has a known service with * permissions
+        THEN all possible actions for the service should be returned
+        """
+        test_policy = '{"Version": "2012-10-17", ' + \
+                      '"Statement": [{"Action": ["service:*"], ' + \
+                      '"Effect": "Allow", ' + \
+                      '"Resource": "*"}]}'
+        test_actions = ['service:action1', 'service:action2', 'service:action3']
+
+        full_policy = expand_permissions.expand_permissions(test_policy, test_actions)
+
+        expected_policy = '{"Version": "2012-10-17", ' + \
+                          '"Statement": [{"Action": ["service:action1", "service:action2", "service:action3"], ' + \
+                          '"Effect": "Allow", ' + \
+                          '"Resource": "*"}]}'
+        self.assertEqual(full_policy, expected_policy)
+
+    def test_partial_service_expansion(self):
+        """
+        WHEN the given policy has a service with partial * actions (e.g. Get*)
+        THEN all matching actions for the service should be returned
+        """
+        test_policy = '{"Version": "2012-10-17", ' + \
+                      '"Statement": [{"Action": ["service:Get*"], ' + \
+                      '"Effect": "Allow", ' + \
+                      '"Resource": "*"}]}'
+        test_actions = ['service:GetAction1', 'service:GetAction2', 'service:PutAction1', 'service:PutAction2']
+
+        full_policy = expand_permissions.expand_permissions(test_policy, test_actions)
+
+        expected_policy = '{"Version": "2012-10-17", ' + \
+                          '"Statement": [{"Action": ["service:GetAction1", "service:GetAction2"], ' + \
+                          '"Effect": "Allow", ' + \
+                          '"Resource": "*"}]}'
+        self.assertEqual(full_policy, expected_policy)
+
+
+if __name__ == '__main__':
+    unittest.main()
